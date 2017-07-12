025dc4d67e9a3b791508af19e4d7a344a95bb977
jonathankim407 <jonathankim407@gmail.com>
2017-07-12 17:31:27 -0400

    add click listener to switch
    when button is hit in switch:
    remove current state class (on)
    apply opposite class (off)
    swap colors of body's background and text
    change text in box

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(event) {
 
-  var status = document.querySelector('.status');
-  status.innerText = "Status text";
+  var lightSwitch = document.querySelector('button');
+
+  lightSwitch.addEventListener('click', function(event) {
+
+    lightSwitch.className = 'switch off';
+
+    var body = document.querySelector('body');
+    body.className = 'dark';
+
+    var status = document.querySelector('.status');
+    status.innerText = 'Hey, who turned off the lights?';
+
+  });
 
 });
